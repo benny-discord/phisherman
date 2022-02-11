a11b6b370c463f7cdc0f9e9ea8a6b3669471788b
Skye <46286597+Skye-31@users.noreply.github.com>
2022-02-11 17:35:05 +0000

    try real int values

--- tests/main.go
+++ tests/main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/benny-discord/phisherman-api"
 )
 
 func main() {
 	key := "a490401c-5582-4866-b3c4-99785abbf845"
 	c := phisherman_api.MakeClient()
-	b := make(phisherman_api.BulkReportDomainBody)
-	b[key] = phisherman_api.BulkReportDomainList{
-		phisherman_api.SuspiciousDomain:            []int{int(time.Now().Unix())},
-		"https://" + phisherman_api.VerifiedDomain: []int{int(time.Now().Unix())},
+	b := phisherman_api.BulkReportDomainBody{
+		key: phisherman_api.BulkReportDomainList{
+			phisherman_api.SuspiciousDomain:            []int{1635591332},
+			"https://" + phisherman_api.VerifiedDomain: []int{1635591333},
+		},
 	}
 	err := c.BulkReportCaughtPhish(b, key)
 	if err != nil {
